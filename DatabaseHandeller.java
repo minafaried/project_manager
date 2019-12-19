9b054265e7241ab392828441d023b48e0b16e14a
Mustafa badr <36304069+moustafaBadrr@users.noreply.github.com>
2019-12-19 20:22:08 +0200

    Update DatabaseHandeller.java
    Comments for each function belongs to his implementer

--- DatabaseHandeller.java
+++ DatabaseHandeller.java
@@ -5,103 +5,108 @@ public class DatabaseHandeller {
 
 	}
 
-	public List<Task> getAllTasks() {
+	public List<Task> getAllTasks() {  // mina
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public Task getTaskByID(int taskID) {
+	public Task getTaskByID(int taskID) {// mina
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public List<SubTask> getAllSubTasks() {
+	public List<SubTask> getAllSubTasks() {// mina
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public SubTask getSubTaskByID(int taskID) {
+	public SubTask getSubTaskByID(int taskID) {// mina
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public List<MileStone> getAllMileStones() {
+	public List<SubTask> getSubTasksOfTask(Task task) { // mina
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public List<MileStone> getAllMileStones() { // Badr
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public MileStone getMileStoneByID(int mileStoneID) {
+	public MileStone getMileStoneByID(int mileStoneID) { // Badr
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public List<TeamMember> getAllTeamMembers() {
+	public List<TeamMember> getAllTeamMembers() {// Badr
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public TeamMember getTeamMemberByID(int teamMemberID) {
+	public MileStone addNewMileStone(MileStone mileStone) { //Badr
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public TeamMember getTeamMemberByID(int teamMemberID) {// Badr
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public List<TeamMember> getTeamMembersOnTask(Task task) {
+	public List<TeamMember> getTeamMembersOnTask(Task task) {// Badr
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public List<TeamMember> getTeamMembersOnSubTask(SubTask subTask) {
+	public List<TeamMember> getTeamMembersOnSubTask(SubTask subTask) {// Badr
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public List<SubTask> getSubTasksOfTask(Task task) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	public void resetDataBase() {
+	public void resetDataBase() { // omar
 		// TODO Auto-generated method stub
 		
 	}
 
-	public Task addNewTask(Task task) {
+	public Task addNewTask(Task task) { // omar
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public SubTask addNewSubTask(Task task, SubTask subTask) {
+	public SubTask addNewSubTask(Task task, SubTask subTask) { // omar
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public MileStone addNewMileStone(MileStone mileStone) {
+	
+
+	public TeamMember addNewTeamMember(TeamMember teamMember) {// omar
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public TeamMember addNewTeamMember(TeamMember teamMember) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void assignTeamMemberToTask(TeamMember teamMember, Task task) {
+	public void assignTeamMemberToTask(TeamMember teamMember, Task task) {// omar
 		// TODO Auto-generated method stub
 		
 	}
 
-	public void assignTeamMemberToSubTask(TeamMember teamMember, SubTask subTask) {
+	public void assignTeamMemberToSubTask(TeamMember teamMember, SubTask subTask) {// omar
 		// TODO Auto-generated method stub
 		
 	}
 
-	public void saveTask(Task task) {
+	public void saveTask(Task task) {// omar
 		// TODO Auto-generated method stub
 		
 	}
 
-	public void saveSubTask(SubTask subTask) {
+	public void saveSubTask(SubTask subTask) { // omar
 		// TODO Auto-generated method stub
 		
 	}
+	
+	
 }
