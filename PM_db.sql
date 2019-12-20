3baaf3502d0ef3d2011f66d372aa544b014edf28
fares09 <36134166+fares09@users.noreply.github.com>
2019-12-20 19:09:08 +0200

    run this to add tables to your database
    make sure to create a database first

---
+++ PM_db.sql
@@ -0,0 +1,45 @@
+CREATE TABLE [Task] (
+  [taskId] int,
+  [workingHours] int,
+  [plannedStartDate] varchar(20),
+  [plannedDueDate] varchar(20),
+  [actualStartDate] varchar(20),
+  [actualDueDate] varchar(20),
+  [name] varchar(20),
+  [parentTaskId] int,
+  PRIMARY KEY ([taskId])
+);
+
+CREATE TABLE [teamMember] (
+  [teamMemberId] int,
+  [name] varchar(20),
+  [title] varchar(20),
+  [workingHours] int,
+  PRIMARY KEY ([teamMemberId])
+);
+
+CREATE TABLE [mileStone] (
+  [mileStoneId] int,
+  [name] varchar(20),
+  [date] varchar(20),
+  PRIMARY KEY ([mileStoneId])
+);
+
+CREATE TABLE [DEPENDS_ON] (
+  [taskId] int REFERENCES Task(taskId),
+  [dependsONTaskId] int REFERENCES Task(taskId)
+  PRIMARY KEY(taskId, dependsONTaskId) 
+
+);
+
+
+CREATE TABLE [WORKS_ON] (
+  [taskId] int REFERENCES Task(taskId),
+  [teamMemberId] int REFERENCES teamMember(teamMemberId) ,
+  [workingDays] int
+
+  PRIMARY KEY(taskId, teamMemberId)
+);
+
+
+
