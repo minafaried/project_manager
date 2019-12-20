8623c3f2dbe6bc48c959f27378d910b819eec469
mina faried <36136028+minafaried@users.noreply.github.com>
2019-12-20 13:34:21 +0200

    add a comparsion functions.
    	// date.compare(d) 
    	//return 1 if date>d
    	//return 0 if date==d
    	//return -1 id date<d

--- Date.java
+++ Date.java
@@ -1,72 +1,105 @@
 import java.util.Scanner;
 
-//The class need the comparsion functions.   //Mina
-public class Date { //The date is in format (dd/mm/yyyy:hh).
+// comparsion functions.   //Mina
+public class Date { // The date is in format (dd/mm/yyyy:hh).
 	private int day;
 	private int month;
 	private int year;
 	private int hours;
-	
+
 	public Date(int day, int month, int year, int hours) {
 		this.day = 0;
 		this.month = 0;
 		this.year = 0;
 		this.hours = 0;
 	}
-	
+
 	public Date() {
 		this(0, 0, 0, 0);
 	}
-	
+
 	public void setDay(int day) {
 		this.day = day;
 	}
-	
+
 	public void setMonth(int month) {
 		this.month = month;
 	}
-	
+
 	public void setYear(int year) {
 		this.year = year;
 	}
-	
+
 	public void setHours(int hours) {
 		this.hours = hours;
 	}
-	
+
 	public int getDay() {
 		return day;
 	}
-	
+
 	public int getMonth() {
 		return month;
 	}
-	
+
 	public int getYear() {
 		return year;
 	}
-	
+
 	public int getHours() {
 		return hours;
 	}
-	
+
 	public void input() {
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.print("Enter the day");
 		day = scanner.nextInt();
-		
+
 		System.out.print("Enter the month");
 		month = scanner.nextInt();
-		
+
 		System.out.print("Enter the year");
 		year = scanner.nextInt();
-		
+
 		System.out.print("Enter the hours");
-		hours = scanner.nextInt(); scanner.nextLine();
+		hours = scanner.nextInt();
+		scanner.nextLine();
 	}
-	
+
 	public String toString() {
 		return day + "/" + month + "/" + year + ":" + hours;
+	}
+
+	// date.compare(d) 
+	//return 1 if date>d
+	//return 0 if date==d
+	//return -1 id date<d
+	public int compare(Date d) {
+		if (this.year > d.year) {
+			return 1;
+		} else if (this.year == d.year) {
+			if (this.month > d.month) {
+				return 1;
+			} else if (this.month == d.month) {
+				if (this.day > d.day) {
+					return 1;
+				} else if (this.day == d.day) {
+					if (this.hours > d.hours) {
+						return 1;
+					} else if (this.hours == d.hours) {
+						return 0;
+					} else {
+						return -1;
+					}
+				} else {
+					return -1;
+				}
+			} else {
+				return -1;
+			}
+		} else {
+			return -1;
+		}
 	}
 }
